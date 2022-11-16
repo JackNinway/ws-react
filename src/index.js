aa3b30b17e7f0f3d94db82297a933a07cfdb0601
Jack Ninway <jack.ninway@gmail.com>
2022-11-16 18:06:40 +0100

    Init hook praktice

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css"
 import DataTable from './DataTable';
+import Skill from './practice/Skill';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<DataTable/>);
+root.render(<Skill/>);
---
+++ src/practice/Skill.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skill = () => {
+    return (
+        <div>
+            <p>===================== Practice Hook =====================</p>
+        </div>
+    );
+};
+
+export default Skill;
